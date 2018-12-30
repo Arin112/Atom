c467c1a463351a9cfa0baf617d29526d6ba313ed
Den <denis.protva@mail.ru>
2018-12-30 14:26:43 +0300

    fix core drawing

--- easyAndBeautiful/main.cpp
+++ easyAndBeautiful/main.cpp
@@ -10,16 +10,25 @@ void adron(double x, double y, double r, int type) {
 		glColor4ub(139, 0, 255, 200);
 	else
 		glColor4f(1, 0, 0, 200.f / 255);
-	for (double d = 0; d <= 2 * M_PI; d += 0.01)
+	for (double d = 0; d <= 2 * M_PI + 0.01; d += 0.01)
 		glVertex2f(x + (r * cos(d)), y + (r * sin(d)));
 	glEnd();
 }
 
 void core(double r) {
-	for (int i = 0; i <= 6; i++) {
+	for (int i = 1; i < 6; i++) {
 		adron(r * cos(i * 2 * M_PI / 6), r * sin(i * 2 * M_PI / 6), r, i % 2);
+		glTranslated(0, 0, -0.00001f);
 	}
+	// tricks with rotation and translation are required for overlapping all
+	// over all
+	glLoadIdentity();
+	glRotatef(1, 1, 0, 0);
+	adron(r, 0, r, 0);
+	glLoadIdentity();
+	glTranslatef(0, 0, -0.1f);
 	adron(0, 0, r, 1);
+	glLoadIdentity();
 }
 
 void electron(double x, double y, double r) {
@@ -52,6 +61,7 @@ int main(int argc, char *argv[]) {
 	glEnable(GL_BLEND);
 	glBlendEquation(GL_FUNC_ADD);
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	glEnable(GL_DEPTH_TEST);
 
 	glutDisplayFunc([]() {}); // empty lambda, do nothing
 
