3454d0f2469e111c41a5685cc81374289bde54f4
Den <denis.protva@mail.ru>
2019-01-11 15:21:27 +0300

    impr

--- tor/main.cpp
+++ tor/main.cpp
@@ -48,22 +48,22 @@ int main(int argc, char *argv[]) {
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
 
-    glTranslatef(0, 1, -10);
+    glTranslatef(0, 1.5, -10);
     glRotatef(90, 1, 0, 0);
     glColor4f(1, 1, 1, 1);
     // glutSolidTorus(0.5, 1, 20, 20);
 
     for (int i = 0; i < 5; i++) {
       glPushMatrix();
-      glTranslatef(0, 0, (sin(t) * 2 * i / 2 + 4 + i * 2) * 0.3);
-      glutSolidTorus(0.3, 0.7 * (sin(t) * i / 2 + 2), 20, 20);
+      glTranslatef(0, 0, (sin(t) * 2 * i / 2 + 6 + i * 2) * 0.3);
+      glutSolidTorus(0.3, 0.3 * (sin(t) * i / 2 + 5), 20, 20);
       glPopMatrix();
     }
-    glTranslatef(0, 0, 2.5);
+    glTranslatef(0, 0, 3.5);
     for (int i = 1; i < 5; i++) {
       glPushMatrix();
-      glTranslatef(0, 0, -(sin(t) * 2 * i / 2 + 4 + i * 2) * 0.3);
-      glutSolidTorus(0.3, 0.7 * (sin(t) * i / 2 + 2), 20, 20);
+      glTranslatef(0, 0, -(sin(t) * 2 * i / 2 + 6 + i * 2) * 0.3);
+      glutSolidTorus(0.3, 0.3 * (sin(t) * i / 2 + 5), 20, 20);
       glPopMatrix();
     }
 
